acbf8e087d99b40f120defe70d0237392eda5bc4
Sebastián <dev1@surgicalcapital.com>
2025-02-12 16:24:54 -0500

    trying things out

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return("Hello World")
+
 @app.get("/employee")
 async def get_employee():
     return {"id": 1, "name": "John Doe"}
 
-# Run using: uvicorn filename:app --reload
+@app.get(api:="/api")
+def hit_api():
+    return {"Arrived at:": api}
